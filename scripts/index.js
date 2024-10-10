f882075d50953b1892aac1fc6df4b1425c59a937
siftiyan <abdulzidan118@gmail.com>
2024-10-10 19:23:31 +0700

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,11 @@ function validateForm() {
   var password = document.getElementById("password").value;
 
   //validasi username dan password, username dan password bisa diganti sesuai keinginan
-  if (username === "user" && password === "user") {
+  if (username === "annisacantik" && password === "annisacantik") {
     Swal.fire({
       icon: "success",
       title: "Login berhasil!",
-      text: "Selamat datang ayanggg",
+      text: "Selamat datang annisaa",
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
